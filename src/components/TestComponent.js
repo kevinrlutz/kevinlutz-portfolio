4d53b0a3d67878ec077384d840bd5dd5d54fdd44
Kevin Lutz <kevinlutz429@gmail.com>
2024-04-28 17:59:36 -0400

    initial project setup

---
+++ src/components/TestComponent.js
@@ -0,0 +1,11 @@
+import './TestComponent.css';
+
+function TestComponent() {
+  return (
+    <div className="App">
+        <h1>Kevin Lutz</h1>
+    </div>
+  );
+}
+
+export default TestComponent;
